747f282b0746cfc2795afc67f6ee4ed0232e2c63
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-30 18:03:18 -0300

    feat: Create redirect to rota2 for rota1

--- routes/web.php
+++ routes/web.php
@@ -39,13 +39,10 @@ Route::get('/rota1', function (){
     echo 'rota 1';
 })->name('site.rota1');
 
-//Route::get('/rota2', function (){
-//    echo 'rota 2';
-//})->name('site.rota2');
-//
+Route::get('/rota2', function (){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
 
-
-Route::redirect('/rota2', 'rota1');
 
 
 
