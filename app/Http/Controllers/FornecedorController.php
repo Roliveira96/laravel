fc18bf30969112a949a6c30c8539940fd15c287f
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 14:37:16 -0300

    feat: Switch case test

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,11 @@ class FornecedorController extends Controller
     function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'fornecedor 1', 'status' => 'n'],
-            1 => ['nome' => 'fornecedor 1', 'status' => 'n']
+            0 => ['nome' => 'fornecedor 1', 'status' => 'n','ddd'=>11,'telefone'=>'8888-9999'],
+            1 => ['nome' => 'fornecedor 2', 'status' => 'n','ddd'=>22,'telefone'=>'8888-9999'],
+            2 => ['nome' => 'fornecedor 3', 'status' => 'n','ddd'=>33,'telefone'=>'8888-9999'],
+            3 => ['nome' => 'fornecedor 4', 'status' => 'n','ddd'=>44,'telefone'=>'8888-9999'],
+            4 => ['nome' => 'fornecedor 5', 'status' => 'n','ddd'=>55,'telefone'=>'8888-9999']
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -9,5 +9,24 @@
     Status: {{ $fornecedores[0]['status'] }}
     <br>
     CNPj: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br>
+    Telefone: {{ $fornecedores[0]['ddd'] ?? '' }}  {{ $fornecedores[0]['telefone'] ?? '' }}
+
+    @switch( $fornecedores[3]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('22')
+            Guarapuva - PR
+            @break
+        @case('33')
+            Rio - PR
+            @break
+        @case('44')
+            Candoi - PR
+            @break
+        @default
+            Cascavel - PR
+    @endswitch
 
 @endisset
