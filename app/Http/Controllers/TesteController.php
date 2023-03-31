febfe00070ffea62fe1058a3cd643b86d52e6ab6
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 10:14:12 -0300

    feat: Comment test

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,7 @@ class TesteController extends Controller
 {
     public function teste(int $valor1, int $valor2)
     {
-        return view('site.teste')->with('valor1', $valor1)->with('valor2',$valor2);
+
+        return view('site.teste', compact('valor1', 'valor2'));
     }
 }
