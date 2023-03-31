97724bc5aad87de61193124392633efe069078f6
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 09:46:07 -0300

    feat: Test passing the values using the compact function

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,7 +8,6 @@ class TesteController extends Controller
 {
     public function teste(int $valor1, int $valor2)
     {
-//        echo "A soma de p1 + p2 é " . $valor1 + $valor2;
-        return view('site.teste', ['valor1' => $valor1, 'valor2'=>$valor2]);
+        return view('site.teste', compact('valor1', 'valor2'));
     }
 }
