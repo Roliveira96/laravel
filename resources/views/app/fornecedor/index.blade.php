a67f3203b5842e088a7aeed94c5474b7314445f7
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 11:42:28 -0300

    feat: Editing the READE.md

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,20 +1,7 @@
 <h3>Fornecedor</h3>
 
-@php
-
-
-    @endphp
-
 
 Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br>
 Status: {{ $fornecedores[0]['status'] }}
 
-@if($fornecedores[0]['status'] == 'n' )
-    <p><b>Fornecedor Inativo</b></p>
-
-@endif
-
-@unless($fornecedores[0]['status'] == 'n')
-    <p><b>Fornecedor Ativo</b></p>
-@endunless
