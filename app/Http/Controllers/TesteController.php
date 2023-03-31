44ce55e929ee0ea9175fa562de061ba56ebbd3d4
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 09:43:56 -0300

    feat: Test by passing an associative array

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,7 @@ class TesteController extends Controller
 {
     public function teste(int $valor1, int $valor2)
     {
-        echo "A soma de p1 + p2 é " . $valor1 + $valor2;
+//        echo "A soma de p1 + p2 é " . $valor1 + $valor2;
+        return view('site.teste', ['valor1' => $valor1, 'valor2'=>$valor2]);
     }
 }
---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,4 @@
+
+<h3>Olá teste</h3>
+A soma de {{ $valor1 }} mais
+{{ $valor2 }}, resulta no valor: {{ $valor1+$valor2 }}
