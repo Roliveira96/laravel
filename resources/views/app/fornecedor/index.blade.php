b66c3a0675cfe0f7dae0170fd08f146de89cf8fd
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2023-03-31 14:44:19 -0300

    feat: For loop test

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,30 +3,18 @@
 
 
 @isset($fornecedores)
-
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPj: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido' }}
-    <br>
-    Telefone: {{ $fornecedores[0]['ddd'] ?? '' }}  {{ $fornecedores[0]['telefone'] ?? '' }}
-
-    @switch( $fornecedores[3]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('22')
-            Guarapuva - PR
-            @break
-        @case('33')
-            Rio - PR
-            @break
-        @case('44')
-            Candoi - PR
-            @break
-        @default
-            Cascavel - PR
-    @endswitch
-
+    @for($i = 0 ; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPj: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido' }}
+        <br>
+        Telefone: {{ $fornecedores[$i]['ddd'] ?? '' }}  {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <br>
+        <hr>
+    @endfor
 @endisset
+
+<br>
+
